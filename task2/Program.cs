816145fb3e5fe5e00dc9534d7b044c1689523214
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-03 17:10:39 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] day = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+Console.WriteLine("Введите номер дня недели (от 1 до 7): ");
+bool text = true;
+while (text)
+{
+    int numberDay = int.Parse(Console.ReadLine());
+    if (1 <= numberDay && numberDay <= 7)
+    {
+        Console.WriteLine("День недели: " + day[numberDay - 1]);
+        text = false;
+    }
+    else
+    {
+        Console.WriteLine("Ввели неверное число, попробуйте снова!");
+    }
+}
+
+
