4e3eee407847b85c86c7c382a95b55de04cf4948
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-04 12:02:09 +0300

    Решение задач №2,3,4 на семминаре

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N. 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 2 -> " -2, -1, 0, 1, 2"
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int index = 0;
+while (index <= Math.Abs(number * 2))
+{
+    if (number > 0)
+    {
+        Console.Write((number * -1) + index);
+    }
+    else
+    {
+        Console.Write(number + index);
+    }
+    Console.Write(" ");
+    index++;
+}
---
+++ task4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 
+// 456 -> 6 
+// 782 -> 2 
+// 918 -> 8
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+while (99 > Math.Abs(number) || 1000 < Math.Abs(number))
+{
+    Console.WriteLine("Введите другое значение!");
+    number = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Последняя цифра числа равна: " + (Math.Abs(number) % 10));
+
+
+// Мой вариант
+// Console.Write("Введите трёхзначное число: ");
+// string number = Console.ReadLine();
+// int index = 2;
+// char value = number[index];
+// Console.WriteLine("Последнее число равно: " + value);
