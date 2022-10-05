f0c17e2f64cf2a69bf6f8a9491d529c4f0d5eb0c
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-05 09:29:13 +0300

    Добавил второй вариант решения задачи в task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,19 +1,51 @@
-﻿string[] day = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
-Console.WriteLine("Введите номер дня недели (от 1 до 7): ");
-bool text = true;
-while (text)
+﻿// Мой первый вариант
+// string[] day = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+// Console.WriteLine("Введите номер дня недели (от 1 до 7): ");
+// bool text = true;
+// while (text)
+// {
+//     int numberDay = int.Parse(Console.ReadLine());
+//     if (1 <= numberDay && numberDay <= 7)
+//     {
+//         Console.WriteLine("День недели: " + day[numberDay - 1]);
+//         text = false;
+//     }
+//     else
+//     {
+//         Console.WriteLine("Ввели неверное число, попробуйте снова!");
+//     }
+// }
+
+
+// Попробовать второй вариант с switch
+Console.Clear();
+Console.Write("Введите день недели: ");
+int numberDay = int.Parse(Console.ReadLine());
+switch (numberDay)
 {
-    int numberDay = int.Parse(Console.ReadLine());
-    if (1 <= numberDay && numberDay <= 7)
-    {
-        Console.WriteLine("День недели: " + day[numberDay - 1]);
-        text = false;
-    }
-    else
-    {
-        Console.WriteLine("Ввели неверное число, попробуйте снова!");
-    }
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница, развратница!");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;
+    default:
+        Console.WriteLine("Введено не корректное значение!");
+        break;
 }
 
-
-// попробовать второй вариант с switch
