c50f86ab8e3d893a0ea02608f9272d78d5e85bc5
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-06 11:11:42 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,25 @@
+﻿int IndexNumber(int number)
+{
+    int result = 0;
+    int index = 1000;
+    while (index < number)
+    {
+        number = number / 10;
+    }
+    return result = number % 10;
+}
+
+Console.Clear();
+Console.Write("Введите любое целое число: ");
+int number;
+while(!int.TryParse(Console.ReadLine(), out number)){
+    Console.WriteLine("Вы ввели не корректное значение! Попробуйте ещё раз.");
+}
+if (Math.Abs(number) > 99)
+{
+    Console.WriteLine(number + " -> " + IndexNumber(Math.Abs(number)));
+}
+else
+{
+    Console.WriteLine(number + " -> третьей цифры нет");
+}
