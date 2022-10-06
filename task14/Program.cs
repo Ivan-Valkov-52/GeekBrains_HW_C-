0bd681fbcc82b0eb61bcef278e77057c5898ce6a
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-06 11:11:19 +0300

    task14

---
+++ task14/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number;
+while(!int.TryParse(Console.ReadLine(), out number)){
+    Console.WriteLine("Вы ввели не корректное значение! Попробуйте ещё раз.");
+}
+bool log = true;
+while (log)
+{
+    if (99 < Math.Abs(number) && Math.Abs(number) < 1000)
+    {
+        int value = (number / 10) % 10;
+        Console.WriteLine(number + " -> " + Math.Abs(value));
+        log = false;
+    }
+    else
+    {
+        Console.WriteLine("Ввели не верное значение!");
+        number = int.Parse(Console.ReadLine());
+    }
+}
+
+
