5d2f85f81719892e32d18f2ada0082d36c5e832c
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-06 11:11:51 +0300

    task16

---
+++ task16/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите день недели: ");
+int numberDay;
+while(!int.TryParse(Console.ReadLine(), out numberDay)){
+    Console.WriteLine("Вы ввели не корректное значение! Попробуйте ещё раз.");
+}
+switch (numberDay)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine(numberDay + " -> Да!");
+        break;
+    case 6:
+    case 7:
+        Console.WriteLine(numberDay + " -> Нет!");
+        break;
+    default:
+        Console.WriteLine("Введено не корректное значение!");
+        break;
+}
