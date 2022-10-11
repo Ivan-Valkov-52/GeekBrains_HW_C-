e263bc1e6278102cf345a80f5fff02593038db50
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-11 15:59:48 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+// 3 -> 1, 8, 27 
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите любое целое число: ");
+int number;
+while (!int.TryParse(Console.ReadLine(), out number))
+    Console.Write("Не верное значение! Попробуйте ещё раз: ");
+Console.Write(number + " -> ");
+for (int index = 1; index <= number; index++)
+{
+    if (index == number)
+    {
+        Console.Write(Math.Pow(index, 3));
+        break;
+    }
+    Console.Write(Math.Pow(index, 3) + ", ");
+}
