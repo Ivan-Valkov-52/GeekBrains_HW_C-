30552ec262ebb0b92cd7bda21ad799fb55c2cdc7
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-11 15:59:27 +0300

    task21

---
+++ task21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> Нет 
+// 23432 -> Да
+// 12821 -> Да
+
+void Palindrome(int number)
+{
+    if (number % 10 == (number / 10000) % 10 && (number / 10) % 10 == (number / 1000) % 10)
+    {
+        Console.WriteLine(number + " -> Да");
+    }
+    else
+    {
+        Console.WriteLine(number + " -> Нет");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите любое пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+Palindrome(number);
+
