3a8d56ff26638a5fbb09c973317255320c159cbb
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-13 16:07:23 +0300

    task26

---
+++ task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N. 
+// 4 -> 24 
+// 5 -> 120
+
+int ProductNumbers (int num){
+    int result = 1;
+    for(int i = 1; i <= num; i++){
+        result *= i;
+    }
+    return result;
+}
+
+Console.Clear();
+Console.Write("Введи любое число: ");
+int number;
+while(!int.TryParse(Console.ReadLine(), out number))
+    Console.Write("Попробуйте ещё раз: ");
+Console.WriteLine($"{number} -> {ProductNumbers(number)}");
