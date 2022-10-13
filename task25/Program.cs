510a5e67c02d4113b18c54291ec7df15cae8152a
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-13 16:07:13 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе. 
+// 456 -> 3 
+// 78 -> 2 
+// 89126 -> 5
+
+int Value(int num)
+{
+    int count = 1;
+    int index = 10;
+    while (index < Math.Abs(num))
+    {
+        count++;
+        index *= 10;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите любое число: ");
+int number;
+while (!int.TryParse(Console.ReadLine(), out number))
+    Console.Write("Введите корректное значение: ");
+Console.WriteLine(number + " -> " + Value(number));
