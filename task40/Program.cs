90348b800fe81c24d7f7d7f2f9965339ef6fc4cc
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-25 17:21:02 +0300

    task40

---
+++ task40/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+
+int ImputNomber()
+{
+    int num;
+    while (!int.TryParse(Console.ReadLine(), out num))
+        Console.Write("Введите корректное значение: ");
+    return num;
+}
+
+void GetTriangle(int sideA, int sideB, int sideC)
+{
+    if (sideA < sideB + sideC)
+    {
+        if (sideB < sideA + sideC)
+        {
+            if (sideC < sideA + sideB)
+            {
+                Console.Write($"Треугольник ABC({sideA},{sideB},{sideC}) - существует!");
+            }
+            else
+            {
+                Console.Write($"Треугольник ABC({sideA},{sideB},{sideC}) - не существует!");
+            }
+        }
+        else
+        {
+            Console.Write($"Треугольник ABC({sideA},{sideB},{sideC}) - не существует!");
+        }
+    }
+    else
+    {
+        Console.Write($"Треугольник ABC({sideA},{sideB},{sideC}) - не существует!");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите длину стороны А треукольника АBC: ");
+int sideA = ImputNomber();
+Console.Write("Введите длину стороны B треукольника АBC: ");
+int sideB = ImputNomber();
+Console.Write("Введите длину стороны C треукольника АBC: ");
+int sideC = ImputNomber();
+GetTriangle(sideA, sideB, sideC);
