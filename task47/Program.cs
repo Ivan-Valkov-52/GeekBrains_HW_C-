36da9d5c14052301e96e74ff8d804d4acfef3987
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-27 09:04:35 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,41 @@
+﻿int InputNumber()
+{
+    int num;
+    while (!int.TryParse(Console.ReadLine(), out num))
+        Console.Write("Введите корректное значение: ");
+    return num;
+}
+
+double[,] GetArray(int line, int column, int minValue, int maxValue)
+{
+    Random rand = new Random();
+    double[,] arr = new double[line, column];
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = Math.Round((rand.Next(minValue, maxValue) + rand.NextDouble()), 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк двумерного массива: ");
+int line = InputNumber();
+Console.Write("Введите количество столбцов двумерного массива: ");
+int column = InputNumber();
+double[,] array = GetArray(line, column, -5, 5);
+PrintArray(array);
