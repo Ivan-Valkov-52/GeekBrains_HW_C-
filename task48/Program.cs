675e6c57caf1f3979261106dbd95ade5edd0efd6
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-27 09:03:27 +0300

    task48

---
+++ task48/Program.cs
@@ -0,0 +1,32 @@
+﻿int[,] GetArray(int line, int column)
+{
+    int[,] arr = new int[line, column];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = j + i;
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int line = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = int.Parse(Console.ReadLine());
+int[,] array = GetArray(line, column);
+PrintArray(array);
