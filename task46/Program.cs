1b1b7f468e9dbe1a10f7cb319fe81091cfb3a9e4
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-27 09:04:28 +0300

    task46

---
+++ task46/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами. 
+// m = 3, n = 4. 
+// 1 4 8 19 
+// 5 -2 33 -2
+// 77 3 8 1
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+Console.Clear();
+GetArray(3, 4, -5, 5);
